d8ae4d453c5c79013ed9d4805bebb63de6c002a5
SableVector <lutchenkoroma@gmail.com>
2022-08-02 08:45:17 +0300

    added plug for IE11

--- testingTask7/js/main.js
+++ testingTask7/js/main.js
@@ -1,4 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // IE check
+    (function () {
+        function bodyReset() {
+            var body = document.querySelector('body');
+            var main = document.createElement('main');
+            var title = document.createElement('h1');
+            var message = document.createElement('p');
+            body.innerHTML = '';
+            main.style = 'width: 100%; padding: 50px; margin: auto;';
+            title.style = 'width: 100%; text-align: center; color: rgb(127,127,127);';
+            title.textContent = "Website's Error";
+            message.style = 'width: 100%; text-align: center; color: rgb(0,0,0);';
+            message.textContent = "Sorry, your browser doesn't support this website. Please, check your browser for updates now or try to use any other modern browsers.";
+            body.appendChild(main);
+            main.appendChild(title);
+            main.appendChild(message);
+        }
+
+        if (navigator.userAgent.indexOf("MSIE") > -1 || navigator.userAgent.indexOf("Trident") > -1) {
+            bodyReset();
+        }
+
+        if (/iP(hone|od|ad)/.test(navigator.platform)) {
+            var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+            if (parseInt(v[1], 10) <= 9) {
+                bodyReset();
+            }
+        }
+    })();
     // Mobile Menu
     const menuBurger = function () {
         if (document.querySelector('#burger')) {
